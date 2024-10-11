abfdd3f1f9e4c04c157714851fe6350c3545738c
Biven444 <biven2010@gmail.com>
2024-10-11 11:24:35 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,15 @@
+from operator import truediv
+
+name = 'Михаил Александрович'
+print(type(name))
+print(name)
+age = 39
+print(age)
+new_age = 40
+print(new_age)
+is_student = True
+is_student1 = True
+print(is_student and is_student1)
+
+
+
